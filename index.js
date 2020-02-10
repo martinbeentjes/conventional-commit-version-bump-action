54760dd7c2703abfce26805a62afbcbe6c9a14ec
Martin Beentjes <martin@beentjes.me>
2020-02-10 16:07:35 +0100

    Determine releaseType based upon conventional commits

--- index.js
+++ index.js
@@ -2,27 +2,13 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 const conventionalRecommendedBump = require(`conventional-recommended-bump`);
 
-
-
 try {
-  // `who-to-greet` input defined in action metadata file
-  const bugfixBranch = core.getInput('bugfix-branch');
-  const featureBranch = core.getInput('feature-branch');
-
-  console.log(`Using ${bugfixBranch} for bugfixes`);
-  console.log(`Using ${featureBranch} for feature releases`);
-
   conventionalRecommendedBump({
     preset: `angular`
   }, (error, recommendation) => {
-    console.log(`Error: ${error}`);
-    console.log(recommendation);
     console.log(`Executing a ${recommendation.releaseType} release`);
+    core.setOutput("next-release-type", recommendation.releaseType);
   });
-
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
 } catch (error) {
   core.setFailed(error.message);
 }
